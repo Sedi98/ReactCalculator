2f56447d9441a71d9b111bf37329a2dccb7f518f
Sedi98 <sedi9816@gmail.com>
2025-02-10 21:03:13 +0400

    fixed critical bug prevents loading

--- src/Components/MiddleComponents/Footer/Footer.jsx
+++ src/Components/MiddleComponents/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import style from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <div className={style.footer}> © 2024{ new Date().getFullYear() > 2024 ? `-${new Date.getFullYear()}` : ''} <a href="https://github.com/Sedi98" target="_blank">Sadi Mammadov</a>. All rights reserved. </div>
+    <div className={style.footer}> © 2024{ new Date().getFullYear() > 2024 ? `-${new Date().getFullYear()}` : ''} <a href="https://github.com/Sedi98" target="_blank">Sadi Mammadov</a>. All rights reserved. </div>
   )
 }
 
